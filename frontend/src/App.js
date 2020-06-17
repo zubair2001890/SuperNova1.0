b2a2d52ff0d566f3e9542d64110dd9d5e63a3085
AkThao <akaash.thao@gmail.com>
2020-06-17 17:35:22 +0100

    Add fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 const App = () => {
     return (
         <div id="root">
-            <div>
-                <h1>FREQUENTLY ASKED QUESTIONS</h1>
+            <div id="header-container">
+                <img></img>
+                <div id="title">
+                    <h1>FREQUENTLY ASKED QUESTIONS</h1>
+                </div>
             </div>
-            <div class="question-block">
-                <h2 class="question">Who is SuperNova for?</h2>
-                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
-            </div>
-            <div class="question-block">
-                <h2 class="question">Can I refund my donation?</h2>
-                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
-            </div>
-            <div class="question-block">
-                <h2 class="question">Can I create more than one project at once?</h2>
-                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+            <div id="q-and-a-s">
+                <div class="question-block">
+                    <h2 class="question">Who is SuperNova for?</h2>
+                    <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+                </div>
+                <div class="question-block">
+                    <h2 class="question">Can I refund my donation?</h2>
+                    <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+                </div>
+                <div class="question-block">
+                    <h2 class="question">Can I create more than one project at once?</h2>
+                    <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+                </div>
             </div>
         </div>
     );
