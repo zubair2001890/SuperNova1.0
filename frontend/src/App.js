4bf2cf6afae95ec589864f8dda8775ea86d24d3c
AkThao <akaash.thao@gmail.com>
2020-06-17 16:39:16 +0100

    Basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div id="root">
+            <div>
+                <h1>FREQUENTLY ASKED QUESTIONS</h1>
+            </div>
+            <div class="question-block">
+                <h2 class="question">Who is SuperNova for?</h2>
+                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+            </div>
+            <div class="question-block">
+                <h2 class="question">Can I refund my donation?</h2>
+                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+            </div>
+            <div class="question-block">
+                <h2 class="question">Can I create more than one project at once?</h2>
+                <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
