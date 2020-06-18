c9ac53040a193b4598bf0a6cd7f750931c90aedf
AkThao <akaash.thao@gmail.com>
2020-06-18 15:00:32 +0100

    Add images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Helmet } from "react-helmet"
 import './App.css';
+import logo from "./images/Stellated-Dodecahedron-white.png"
 
 const TITLE = "FAQs"
 
 const App = () => {
     return (
         <div id="root">
-            <Helmet>
-                <title>{ TITLE }</title>
-            </Helmet>
-            <div id="header-container">
-                <img></img>
+            <header id="header-container">
+                <div id="menu-icon"></div>
+                <div id="header-logo"><a href="http://supernova.ac/"><img src={logo} id="logo"/></a></div>
+                <div id="header-links"></div>
                 <div id="title">
                     <h1>FREQUENTLY ASKED QUESTIONS</h1>
                 </div>
-            </div>
-            <div id="main">
+            </header>
+            <main id="main">
                 <div class="question-block">
                     <h2 class="question">Who is SuperNova for?</h2>
                     <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
@@ -29,7 +28,7 @@ const App = () => {
                     <h2 class="question">Can I create more than one project at once?</h2>
                     <p class="answer">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a ligula ut, euismod convallis sem. Mauris fermentum felis in nibh egestas pretium. Integer hendrerit urna mi, ut facilisis magna placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus. Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat. Nullam eu accumsan justo.</p>
                 </div>
-            </div>
+            </main>
         </div>
     );
 }
