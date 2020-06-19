d4386deca8aef7b57483a70da8ced2b04d764276
AkThao <akaash.thao@gmail.com>
2020-06-19 13:45:24 +0100

    Basic header template

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,37 +1,50 @@
 import React from 'react';
-import { AppBar, Typography, IconButton, Button, Toolbar, Box, Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Typography, IconButton, Button, Toolbar, Box, Link, Avatar } from '@material-ui/core';
+import { makeStyles, } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import logo from "../images/Stellated-Dodecahedron-white.png"
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-        width: theme.spacing("100%"),
-        justifyContent: "space-between",
+        display: "flex",
+        backgroundColor: "black",
+        opacity: 1, // Change to zero once background image is added
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginRight: 16,
+        marginLeft: -12,
+        color: "white",
     },
     link: {
-        flexGrow: 1,
         marginLeft: theme.spacing(2),
         marginRight: theme.spacing(2),
+        color: "white",
     },
+    rightToolbar: {
+        marginLeft: "auto",
+        marginRight: -12,
+    },
+    logo: {
+        marginLeft: "44%",
+    }
 }));
 
 export default function Header() {
     const classes = useStyles();
 
     return (
-        <header className={classes.root}>
-            <AppBar className={classes.root} position="static">
+        <header>
+            <AppBar position="static" className={classes.root}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton className={classes.menuButton} aria-label="menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography>
-                        <Link href="#" className={classes.link} color="inherit">EXPLORE</Link>
-                        <Link href="#" className={classes.link} color="inherit">LOG IN</Link>
+                    <IconButton className={classes.logo} aria-label="logo">
+                        <Avatar src={logo} />
+                    </IconButton>
+                    <Typography className={classes.rightToolbar}>
+                        <Link href="#" className={classes.link}>EXPLORE</Link>
+                        <Link href="#" className={classes.link}>LOG IN</Link>
                     </Typography>
                 </Toolbar>
             </AppBar>
