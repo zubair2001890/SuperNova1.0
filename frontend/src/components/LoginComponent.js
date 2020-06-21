0abb02d583b2054b7d0d69c1ba3840d1459740ee
Danbass07 <danbass666@googlemail.com>
2020-06-21 01:37:03 +0100

    fearure-login-component

--- frontend/src/components/LoginComponent.js
+++ frontend/src/components/LoginComponent.js
@@ -1,142 +1,42 @@
 import React from "react";
 // @material-ui/core components
 import { ThemeProvider } from "@material-ui/core/styles";
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
 import Typography from "@material-ui/core/Typography";
-// @material-ui/icons
-//import People from "@material-ui/icons/People";
-import Email from "@material-ui/icons/Email";
+
 // core components
 import GridContainer from "./Grid/GridContainer.js";
-import GridItem from "./Grid/GridItem.js";
-import Card from "./Card/Card.js";
-import CardHeader from "./Card/CardHeader.js";
-import CardBody from "./Card/CardBody.js";
-import CardFooter from "./Card/CardFooter.js";
-import CustomInput from "./CustomInput/CustomInput.js";
-import Button from "@material-ui/core/Button";
-//import CardContent from "@material-ui/core/CardContent";
-import styles from "./loginStyle.js";
+
 import theme from "./theme.js";
 
-const useStyles = makeStyles(styles);
-
 export default function LoginComponent() {
-  const classes = useStyles();
-
+  const section = {
+    minHeight: "110vh",
+    maxHeight: "1600px",
+    overflow: "hidden",
+    padding: "70px 0",
+    backgroundPosition: "top center",
+    backgroundSize: "cover",
+    margin: "0",
+    border: "0",
+    display: "flex",
+    alignItems: "center",
+    // backgroundImage: "url(" + image + ")",
+  };
+  const conatiner = {
+    paddingRight: "15px",
+    paddingLeft: "15px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    width: "100%",
+  };
   return (
     <ThemeProvider theme={theme}>
-      <div className={classes.section}>
-        <div className={classes.container}>
+      <div style={section}>
+        <div style={conatiner}>
           <GridContainer justify="center">
-            <GridItem xs={12} sm={10} md={8}>
-              <Card className={classes.root}>
-                <form className={classes.form}>
-                  <CardHeader
-                    variant="outlined"
-                    color="primary"
-                    className={classes.cardHeader}
-                  >
-                    <Typography variant="h4" align={"center"}>
-                      Login
-                    </Typography>
-                    <div className={classes.socialLine}>
-                      <Button
-                        justicon="true"
-                        href="#pablo"
-                        target="_blank"
-                        color="secondary"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        <i className={"fa fab fa-twitter fa-2x"} />
-                      </Button>
-                      <Button
-                        justicon="true"
-                        href="#pablo"
-                        target="_blank"
-                        color="secondary"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        <i className={"fa fa-facebook fa-2x"} />
-                      </Button>
-                      <Button
-                        justicon="true"
-                        href="#pablo"
-                        target="_blank"
-                        color="secondary"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        <i
-                          style={{ fontSize: "32px" }}
-                          className={"fab fa-google-plus-g"}
-                        />
-                      </Button>
-                    </div>
-                  </CardHeader>
-                  <CardBody>
-                    {/* <CustomInput
-                      labelText="First Name..."
-                      id="first"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        type: "text",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <People className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        ),
-                      }}
-                    /> */}
-                    <CustomInput
-                      labelText="Email..."
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        type: "email",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Email className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Password"
-                      id="pass"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        type: "password",
-
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off",
-                      }}
-                    />
-                  </CardBody>
-                  <CardFooter>
-                    <Button simple="true" color="primary" size="large">
-                      Sign In
-                    </Button>
-                    <Button simple="true" color="primary" size="large">
-                      Forgot Password
-                    </Button>
-                  </CardFooter>
-                </form>
-              </Card>
-            </GridItem>
+            <Typography variant="h1" gutterBottom>
+              LOGIN
+            </Typography>
           </GridContainer>
         </div>
       </div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,13 +3,12 @@ import { Link as MaterialLink } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import paths from "../constants/paths";
+import LoginComponent from "../components/LoginComponent";
 
 export default function Links() {
   return (
     <>
-      <Typography variant="h1" gutterBottom>
-        Login Page
-      </Typography>
+      <LoginComponent></LoginComponent>
       <Typography>
         <MaterialLink component={Link} to={paths.home}>
           Homepage
