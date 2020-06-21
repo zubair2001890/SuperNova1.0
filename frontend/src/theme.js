0282d95387a0a1365ed72f85cece00dd2c1c9a8d
tacotoemeck <wagner.tomek@gmail.com>
2020-06-21 14:42:22 +0100

    Add palette variables to creatmuiTheme in theme.js
    Relates #25

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,24 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
+// colors
+const redColor = "(255,0,0)"; // red
+const blueColor = "(0,6,71)"; // our blue
+const whiteColor = "(255,255,255)"; // black
+const blackColor = "(0,0,0)"; // white
+
 export default createMuiTheme({
-    // Customize MU theme properties here;
-    // check default theme here https://material-ui.com/customization/default-theme/#default-theme;
+  // Customize MU theme properties here;
+  // check default theme here https://material-ui.com/customization/default-theme/#default-theme;
+  palette: {
+    primary: {
+      main: blueColor,
+    },
+    secondary: {
+      main: redColor,
+    },
+    text: {
+      primary: blackColor,
+      secondary: whiteColor,
+    },
+  },
 });
