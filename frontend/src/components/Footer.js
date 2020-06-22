be6f9f8bbd96fdfbbfe2af367f767b943916faaa
AkThao <akaash.thao@gmail.com>
2020-06-22 16:11:20 +0100

    Add bottom social media navbar

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+// import { FacebookIcon, TwitterIcon, InstagramIcon, LinkedInIcon } from "@material-ui/icons";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        color: "white",
+    },
+    bottomNavBar: {
+        backgroundColor: "black",
+    },
+    footer: {
+        width: "100%",
+        position: "absolute",
+        bottom: "0",
+    }
+}));
+
+export default function Footer() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.footer}>
+            <BottomNavigation className={classes.bottomNavBar}>
+                <BottomNavigationAction className={classes.icon} icon={<FacebookIcon />} />
+                <BottomNavigationAction className={classes.icon} icon={<TwitterIcon />} />
+                <BottomNavigationAction className={classes.icon} icon={<InstagramIcon />} />
+                <BottomNavigationAction className={classes.icon} icon={<LinkedInIcon />} />
+            </BottomNavigation>
+        </div>
+    )
+}
