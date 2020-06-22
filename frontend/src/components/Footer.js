0464286e8ce9eb3988c90e80fa36cfad12757b55
AkThao <akaash.thao@gmail.com>
2020-06-22 19:45:40 +0100

    Add all text links to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    Grid,
+    Divider,
+    Link,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import {
     Facebook as FacebookIcon,
@@ -20,7 +26,17 @@ const useStyles = makeStyles((theme) => ({
         width: "100%",
         position: "absolute",
         bottom: "0",
-    }
+        backgroundColor: "black",
+    },
+    regularLink: {
+        color: "white",
+    },
+    boldLink: {
+        color: "white",
+    },
+    gridItem: {
+        textAlign: "center",
+    },
 }));
 
 export default function Footer() {
@@ -28,6 +44,46 @@ export default function Footer() {
 
     return (
         <div className={classes.footer}>
+            <Grid container style={{ paddingBottom: "3%"}}>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.boldLink} className={classes.boldLink}>ABOUT</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.boldLink}>CREATE</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.boldLink}>EXPLORE</Link>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} style={{ paddingBottom: "2%"}}>
+                <Grid item xs={4} className={classes.gridItem} className={classes.gridItem}>
+                    <Link className={classes.regularLink} className={classes.regularLink}>Our Mission</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Upload a Project</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Browse Projects</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>T&Cs</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Create Account</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Funded Projects</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Our Model</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>Contact Us</Link>
+                </Grid>
+                <Grid item xs={4} className={classes.gridItem}>
+                    <Link className={classes.regularLink}>FAQ</Link>
+                </Grid>
+            </Grid>
             <BottomNavigation className={classes.bottomNavBar}>
                 <BottomNavigationAction className={classes.icon} icon={<FacebookIcon />} />
                 <BottomNavigationAction className={classes.icon} icon={<TwitterIcon />} />
