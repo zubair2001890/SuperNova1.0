395fa96a2cc26d7a70ba860ed45d01050de16214
tacotoemeck <wagner.tomek@gmail.com>
2020-06-22 08:31:04 +0100

    Add monteserrat font to muiThemes
    Relates #25

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -6,6 +6,12 @@ const blueColor = "(0,6,71)"; // our blue
 const whiteColor = "(255,255,255)"; // black
 const blackColor = "(0,0,0)"; // white
 
+// fonts
+
+const monteserrat = "font-family: 'Montserrat', sans-serif;";
+// Ava Meridian - needs to be added
+// Averta Demo - needd to be added
+
 export default createMuiTheme({
   // Customize MU theme properties here;
   // check default theme here https://material-ui.com/customization/default-theme/#default-theme;
@@ -19,6 +25,26 @@ export default createMuiTheme({
     text: {
       primary: blackColor,
       secondary: whiteColor,
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: monteserrat,
+    },
+    h2: {
+      fontFamily: monteserrat,
+    },
+    h3: {
+      fontFamily: monteserrat,
+    },
+    h4: {
+      fontFamily: monteserrat,
+    },
+    h5: {
+      fontFamily: monteserrat,
+    },
+    h6: {
+      fontFamily: monteserrat,
     },
   },
 });
