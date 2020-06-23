71ee5337b679dac81694199f26ae2deb4851aef0
tacotoemeck <wagner.tomek@gmail.com>
2020-06-23 08:40:34 +0100

    Fix reviewer comments and add new font Relates #27

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,50 +1,74 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 // colors
-const redColor = "(255,0,0)"; // red
-const blueColor = "(0,6,71)"; // our blue
-const whiteColor = "(255,255,255)"; // black
-const blackColor = "(0,0,0)"; // white
+const appColors = {
+  red: "#FF0000",
+  blue: "#000647",
+  black: "#FFFFFF",
+  white: "#000000",
+};
 
 // fonts
-
-const monteserrat = "font-family: 'Montserrat', sans-serif;";
-// Ava Meridian - needs to be added
-// Averta Demo - needd to be added
+const appFonts = {
+  monteserrat: "font-family: 'Montserrat', sans-serif;",
+  avertaDemo: "font-family: 'AvertaDemo', sans-serif;",
+};
 
 export default createMuiTheme({
   // Customize MU theme properties here;
   // check default theme here https://material-ui.com/customization/default-theme/#default-theme;
   palette: {
     primary: {
-      main: blueColor,
+      main: appColors.blue,
     },
     secondary: {
-      main: redColor,
+      main: appColors.red,
     },
     text: {
-      primary: blackColor,
-      secondary: whiteColor,
+      primary: appColors.black,
+      secondary: appColors.white,
     },
   },
   typography: {
+    fontFamily: appFonts.avertaDemo,
     h1: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
     },
     h2: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
     },
     h3: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
     },
     h4: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
     },
     h5: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
     },
     h6: {
-      fontFamily: monteserrat,
+      fontFamily: appFonts.monteserrat,
+    },
+    subtitle1: {
+      fontFamily: appFonts.monteserrat,
+    },
+    subtitle2: {
+      fontFamily: appFonts.monteserrat,
+    },
+    body1: {
+      fontFamily: appFonts.avertaDemo,
+    },
+    body2: {
+      fontFamily: appFonts.avertaDemo,
+    },
+    button: {
+      fontFamily: appFonts.avertaDemo,
+    },
+    caption: {
+      fontFamily: appFonts.avertaDemo,
+    },
+    overline: {
+      fontFamily: appFonts.avertaDemo,
     },
   },
 });
