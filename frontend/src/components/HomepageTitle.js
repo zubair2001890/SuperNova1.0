f1664a3024b9f6a14379e76caccb59fc0e2e7131
AkThao <akaash.thao@gmail.com>
2020-06-25 17:58:39 +0100

    Position and style title

--- frontend/src/components/HomepageTitle.js
+++ frontend/src/components/HomepageTitle.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Typography } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        fontFamily: "avaMeridian",
+        textAlign: "center",
+        fontSize: "7.45vw",
+        letterSpacing: 10.01,
+    },
+}));
 
 export default function HomepageTitle() {
+    const classes = useStyles();
+
     return (
         <>
-            <Typography>SUPERNOVA</Typography>
+            <Typography className={classes.title}>SUPERNOVA</Typography>
         </>
     )
 }
