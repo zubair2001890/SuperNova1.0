86ae6c040f65d1c8d09ba0d733f20bb693649ea8
AkThao <akaash.thao@gmail.com>
2020-06-25 18:06:40 +0100

    Add fade transition to title

--- frontend/src/components/HomepageTitle.js
+++ frontend/src/components/HomepageTitle.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Typography } from '@material-ui/core';
+import { Typography, Fade } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-        fontFamily: "avaMeridian",
-        textAlign: "center",
-        fontSize: "7.45vw",
-        letterSpacing: 10.01,
-    },
+  title: {
+    fontFamily: "avaMeridian",
+    textAlign: "center",
+    fontSize: "7.45vw",
+    letterSpacing: 10.01,
+  },
 }));
 
 export default function HomepageTitle() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <>
-            <Typography className={classes.title}>SUPERNOVA</Typography>
-        </>
-    )
+  return (
+    <>
+      <Fade in={true} timeout={1500}>
+        <Typography className={classes.title}>SUPERNOVA</Typography>
+      </Fade>
+    </>
+  );
 }
