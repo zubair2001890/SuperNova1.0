79673c4a5a15481cae17f01f8234ba9f49702c47
tchappel <tchappel@ebay.com>
2020-06-27 00:46:23 +0200

    remove theme provider

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link as MaterialLink } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/styles";
 import { Grid, Typography } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 import paths from "../constants/paths";
@@ -10,19 +9,16 @@ import theme from "../theme";
 export default function Links() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Grid
-          container
-          style={{ margin: "auto auto" }}
-          item
-          xs={12}
-          sm={6}
-          lg={4}
-        >
-          <LoginForm />
-        </Grid>
-      </ThemeProvider>
-
+      <Grid
+        container
+        style={{ margin: "auto auto" }}
+        item
+        xs={12}
+        sm={6}
+        lg={4}
+      >
+        <LoginForm />
+      </Grid>
       <Typography>
         <MaterialLink component={RouterLink} to={paths.home}>
           Homepage
