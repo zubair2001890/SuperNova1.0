8736c17feadfa45bb0cd82d37a318a0edc16468f
Reeshul <reeshul@hotmail.com>
2020-06-30 11:26:35 +0100

    Added static pages (#60)
    * Added static pages
    
    * Fixed routing
    
    * Updated routing
    
    * Fixed routing finally

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import { Typography, makeStyles } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { setDarkTheme as setPageDarkTheme } from "../store/slices/page";
+
+const useStyles = makeStyles((theme) => ({
+  center: {
+    margin: "0 auto",
+    maxWidth: 966,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingTop: 92,
+    paddingBottom: 92,
+  },
+  mockAppBarLayout: {
+    ...theme.mixins.appBar,
+  },
+  pageHeader: {
+    height: 370,
+    backgroundImage: "url(" + require("./assets/contact.png") + ")",
+  },
+  pageTitle: {
+    position: "relative",
+    top: 20,
+    color: "white",
+  },
+  subheadingVerticalMargin: {
+    margin: "68px 0",
+  },
+  subheadingTextVerticalMargin: {
+    paddingBottom: 48,
+  },
+}));
+
+function Contact() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setPageDarkTheme(true));
+    return () => {
+      dispatch(setPageDarkTheme(false));
+    };
+  }, [dispatch]);
+  return (
+    <>
+      <div className={classes.pageHeader}>
+        <div className={classes.mockAppBarLayout} />
+        <Typography variant="h1" align="center" className={classes.pageTitle}>
+          CONTACT US
+        </Typography>
+      </div>
+      <div className={classes.center}>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          You can reach us at the following addresses:
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          research@supernova.ac
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          wyf@supernova.ac
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          zm@supernova.ac
+        </Typography>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
--- frontend/src/pages/Faq.js
+++ frontend/src/pages/Faq.js
@@ -1,94 +1,133 @@
-import React from "react";
-import { Typography, Box } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { Typography, makeStyles } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { setDarkTheme as setPageDarkTheme } from "../store/slices/page";
 
-function Title() {
-  return (
-    <>
-      <Box my={10}>
-        <Typography variant="h4" align="center">
-          FREQUENTLY ASKED QUESTIONS
-        </Typography>
-      </Box>
-    </>
-  );
-}
-
-function Main() {
-  return (
-    <>
-      <main>
-        <Box my={5} mx="auto" width="50%">
-          <Typography variant="h5">Who is SuperNova for?</Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
-            ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
-            egestas pretium. Integer hendrerit urna mi, ut facilisis magna
-            placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
-            Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
-            Nullam eu accumsan justo.
-          </Typography>
-        </Box>
-        <Box my={5} mx="auto" width="50%">
-          <Typography variant="h5">Can I refund my donation?</Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
-            ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
-            egestas pretium. Integer hendrerit urna mi, ut facilisis magna
-            placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
-            Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
-            Nullam eu accumsan justo.
-          </Typography>
-        </Box>
-        <Box my={5} mx="auto" width="50%">
-          <Typography variant="h5">
-            Can I create more than one project at once?
-          </Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
-            ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
-            egestas pretium. Integer hendrerit urna mi, ut facilisis magna
-            placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
-            Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
-            Nullam eu accumsan justo.
-          </Typography>
-        </Box>
-        <Box my={5} mx="auto" width="50%">
-          <Typography variant="h5">Question question question?</Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
-            ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
-            egestas pretium. Integer hendrerit urna mi, ut facilisis magna
-            placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
-            Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
-            Nullam eu accumsan justo.
-          </Typography>
-        </Box>
-        <Box my={5} mx="auto" width="50%">
-          <Typography variant="h5">Question question question?</Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
-            ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
-            egestas pretium. Integer hendrerit urna mi, ut facilisis magna
-            placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
-            Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
-            Nullam eu accumsan justo.
-          </Typography>
-        </Box>
-      </main>
-    </>
-  );
-}
+const useStyles = makeStyles((theme) => ({
+  center: {
+    margin: "0 auto",
+    maxWidth: 966,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingTop: 92,
+    paddingBottom: 92,
+  },
+  mockAppBarLayout: {
+    ...theme.mixins.appBar,
+  },
+  pageHeader: {
+    height: 370,
+    backgroundImage: "url(" + require("./assets/faq.png") + ")",
+  },
+  pageTitle: {
+    position: "relative",
+    top: 20,
+    color: "white",
+  },
+  subheadingVerticalMargin: {
+    margin: "68px 0",
+  },
+  subheadingTextVerticalMargin: {
+    paddingBottom: 48,
+  },
+}));
 
 function Faq() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setPageDarkTheme(true));
+    return () => {
+      dispatch(setPageDarkTheme(false));
+    };
+  }, [dispatch]);
+
   return (
     <>
-      <Title />
-      <Main />
+      <div className={classes.pageHeader}>
+        <div className={classes.mockAppBarLayout} />
+        <Typography variant="h1" align="center" className={classes.pageTitle}>
+          FREQUENTLY ASKED QUESTIONS
+        </Typography>
+      </div>
+      <div className={classes.center}>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          Who is SuperNova for?
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
+          ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
+          egestas pretium. Integer hendrerit urna mi, ut facilisis magna
+          placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
+          Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
+          Nullam eu accumsan justo.
+        </Typography>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          Can I refund my donation?
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
+          ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
+          egestas pretium. Integer hendrerit urna mi, ut facilisis magna
+          placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
+          Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
+          Nullam eu accumsan justo.
+        </Typography>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          Can I create more than one project at once?
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
+          ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
+          egestas pretium. Integer hendrerit urna mi, ut facilisis magna
+          placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
+          Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
+          Nullam eu accumsan justo.
+        </Typography>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          Question question question?
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
+          ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
+          egestas pretium. Integer hendrerit urna mi, ut facilisis magna
+          placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
+          Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
+          Nullam eu accumsan justo.
+        </Typography>
+        <Typography variant="h2" className={classes.subheadingVerticalMargin}>
+          Question question question?
+        </Typography>
+        <Typography
+          variant="body1"
+          className={classes.subheadingTextVerticalMargin}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          sagittis lectus ac aliquet egestas. Aenean turpis risus, aliquet a
+          ligula ut, euismod convallis sem. Mauris fermentum felis in nibh
+          egestas pretium. Integer hendrerit urna mi, ut facilisis magna
+          placerat et. Aenean sit amet ipsum vitae velit volutpat dapibus.
+          Pellentesque sed neque et ipsum tincidunt volutpat id sit amet erat.
+          Nullam eu accumsan justo.
+        </Typography>
+      </div>
     </>
   );
 }
