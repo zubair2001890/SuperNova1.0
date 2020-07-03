cc904880ebaaf17301f902ecf39fd86a7b6c7c39
AkThao <akaash.thao@gmail.com>
2020-07-03 21:17:57 +0100

    Control positioning of cube upon scroll

--- frontend/src/pages/Homepage/components/Cube.js
+++ frontend/src/pages/Homepage/components/Cube.js
@@ -1,34 +1,76 @@
 import React from "react";
-import { makeStyles, Box } from "@material-ui/core";
-import IntersectionObserver from "../../../components/IntersectionObserver";
+import { makeStyles } from "@material-ui/core";
 
-export default function Cube() {
+const useStyles = makeStyles((theme) => ({
+  cubeSection: {
+    // display: "flex",
+    // flexDirection: "row",
+    // justifyContent: "center",
+    // alignItems: "top",
+    height: "3000px",
+    backgroundColor: "#000647",
+  },
+  cube: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    transformStyle: "preserve-3d",
+    transform: "translateZ(-100px)",
+  },
+  cubeFace: {
+    position: "absolute",
+    width: 200,
+    height: 200,
+    textAlign: "center",
+    backgroundColor: "white",
+    color: "black",
+    opacity: 0.5,
+    border: "3px solid green",
+    borderRadius: 10,
+  },
+}));
+
+export default function Cube({scrollY}) {
+  const classes = useStyles();
+
+  if (scrollY > 4770 && scrollY < 6000) {
+    document.getElementById("cube-container").classList.remove("cube-container");
+    document.getElementById("cube-container").classList.add("cube-container-fixed");
+  }
+  else if (document.getElementById("cube-container") && (scrollY >= 6000 || scrollY <= 4770)) {
+    document.getElementById("cube-container").classList.remove("cube-container-fixed");
+    document.getElementById("cube-container").classList.add("cube-container");
+  }
+
   return (
-    <IntersectionObserver
-      threshold={1}
-      render={({ inView, ref }) => {
-        return (
-          <Box ref={ref}>
-            {inView && (
-              <div
-                className="cubeContainer"
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  height: "800px",
-                }}
-              ></div>
-            )}
-          </Box>
-        );
-      }}
-    />
+    <div className={classes.cubeSection} id="cube-section">
+      <div class="cube-container" id="cube-container">
+        <div className={classes.cube}>
+          <div className={classes.cubeFace} id="cube-face-front">
+            front
+          </div>
+          <div className={classes.cubeFace} id="cube-face-back">
+            back
+          </div>
+          <div className={classes.cubeFace} id="cube-face-right">
+            right
+          </div>
+          <div className={classes.cubeFace} id="cube-face-left">
+            left
+          </div>
+          <div className={classes.cubeFace} id="cube-face-top">
+            top
+          </div>
+          <div className={classes.cubeFace} id="cube-face-bottom">
+            bottom
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
 /* <div
-  style={{
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
