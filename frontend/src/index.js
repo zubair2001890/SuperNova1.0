b1f8bf3bbb9e530330b3b06395bfa2120f83ec43
Eduardo Valencia <8025918@gmail.com>
2020-07-25 14:49:23 -0700

    Format index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import store from './store'
-import { Provider } from 'react-redux'
-import { Auth0Provider } from '@auth0/auth0-react'
-import * as serviceWorker from './serviceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Auth0Provider
@@ -17,10 +17,11 @@ ReactDOM.render(
       <App />
     </Provider>
   </Auth0Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
+
