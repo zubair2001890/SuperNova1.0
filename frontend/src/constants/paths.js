361386694864dd83c2a96e9939c71c86f97b79e6
Eduardo Valencia <8025918@gmail.com>
2020-07-25 14:41:53 -0700

    Format paths

--- frontend/src/constants/paths.js
+++ frontend/src/constants/paths.js
@@ -1,20 +1,20 @@
-import { getAccountUrl } from '../helpers/createPaths'
+import { getAccountUrl } from "../helpers/createPaths";
 
 export default {
-  login: '/login',
-  logout: '/logout',
-  error404: '/not-found',
-  home: '/',
-  explore: '/explore',
-  faq: '/faq',
-  mission: '/ourmission',
-  terms: '/termsandconditions',
-  model: '/ourmodel',
-  contact: '/contactus',
-  team: '/ourteam',
-  informationPageX: '/topicx',
-  account: getAccountUrl(''),
-  myProjects: getAccountUrl('my-projects'),
-  profile: getAccountUrl('profile'),
-  activity: getAccountUrl('activity'),
-}
+  login: "/login",
+  logout: "/logout",
+  error404: "/not-found",
+  home: "/",
+  explore: "/explore",
+  faq: "/faq",
+  mission: "/ourmission",
+  terms: "/termsandconditions",
+  model: "/ourmodel",
+  contact: "/contactus",
+  team: "/ourteam",
+  informationPageX: "/topicx",
+  account: getAccountUrl(""),
+  myProjects: getAccountUrl("my-projects"),
+  profile: getAccountUrl("profile"),
+  activity: getAccountUrl("activity")
+};
