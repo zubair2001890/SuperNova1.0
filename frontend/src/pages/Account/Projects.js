d6e78aa7000d24c13a39150f05b99d4910395442
Eduardo Valencia <8025918@gmail.com>
2020-07-25 15:00:24 -0700

    Format Projects.js

--- frontend/src/pages/Account/Projects.js
+++ frontend/src/pages/Account/Projects.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { withAuth0 } from '@auth0/auth0-react'
-import { makeStyles } from '@material-ui/core'
-import ProjectCard from '../../components/ProjectCard'
-import Body from '../../components/ProjectCard/components/Body'
+import React from "react";
+import { connect } from "react-redux";
+import { withAuth0 } from "@auth0/auth0-react";
+import { makeStyles } from "@material-ui/core";
+import ProjectCard from "../../components/ProjectCard";
+import Body from "../../components/ProjectCard/components/Body";
 
-const marginRight = '1rem'
+const marginRight = "1rem";
 
 const useStyles = makeStyles((theme) => ({
   projects: {
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      flexWrap: 'wrap',
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+      flexWrap: "wrap",
     },
   },
   card: {
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       flexBasis: `calc(50% - ${marginRight})`,
       marginRight,
     },
-    [theme.breakpoints.up('xl')]: {
+    [theme.breakpoints.up("xl")]: {
       flexBasis: `calc(25% - ${marginRight})`,
     },
-    marginBottom: '3.25rem',
+    marginBottom: "3.25rem",
   },
-}))
+}));
 
 const renderProject = (classes) => (projectData, index) => (
   <ProjectCard
@@ -33,23 +33,23 @@ const renderProject = (classes) => (projectData, index) => (
     body={<Body project={projectData} />}
     id={projectData.id}
   ></ProjectCard>
-)
+);
 
 const renderProjects = (projects, classes) =>
-  projects.map(renderProject(classes))
+  projects.map(renderProject(classes));
 
 function Projects({ projects, filterProjects, auth0 }) {
-  const classes = useStyles()
-  const filteredProjects = filterProjects(auth0.user, projects)
+  const classes = useStyles();
+  const filteredProjects = filterProjects(auth0.user, projects);
   return (
     <div className={classes.projects}>
       {projects && renderProjects(filteredProjects, classes)}
     </div>
-  )
+  );
 }
 
-const WithAuth = withAuth0(Projects)
+const WithAuth = withAuth0(Projects);
 
-const mapStateToProps = ({ projects }) => ({ projects: projects.data })
+const mapStateToProps = ({ projects }) => ({ projects: projects.data });
 
-export default connect(mapStateToProps)(WithAuth)
+export default connect(mapStateToProps)(WithAuth);
