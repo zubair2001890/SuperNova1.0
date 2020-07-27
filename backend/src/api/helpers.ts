ba4bab05d31edb90112695e5778b26bb91a133d7
Michael Nares <michaelnares@hotmail.co.uk>
2020-07-27 16:40:12 +0100

    The ProjectBacker and Comment collections have now been added in.

--- backend/src/api/helpers.ts
+++ backend/src/api/helpers.ts
@@ -1,27 +1,3 @@
-export const getFieldForNumber = function(fieldID: number)
-{
-    let fieldsArray = [];
-    fieldsArray[1] = "Biology";
-    fieldsArray[2] = "Physics";
-    fieldsArray[3] = "Social Sciences";
-    fieldsArray[4] = "Mathematics and Statistics";
-    fieldsArray[5] = "Space Sciences";
-    fieldsArray[6] = "Computer Science";
-    fieldsArray[7] = "Engineering and Technology";
-    fieldsArray[8] = "Earth Sciences";
-    return fieldsArray[fieldID];
-}
-
-export const getSubFieldForNumber = function subFieldID(subFieldID: number)
-{
-    let subFieldsArray = [];
-    for (let i = 1; i < 20; i++)
-    {
-        subFieldsArray[i] = "Sub field " + i.toString;
-    }
-    return subFieldsArray[subFieldID];
-}
-
 export const addStringToArray = function (stringArray: Array<String>, newString: String)
 {   let currentArray = new Array();
     if (stringArray !== null && stringArray !== undefined)
