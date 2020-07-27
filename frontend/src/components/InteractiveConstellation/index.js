f0e059d803a175f46a917ef779a639e10431773b
AkThao <akaash.thao@gmail.com>
2020-07-27 16:23:05 +0100

    Refactor star in separate component

--- frontend/src/components/InteractiveConstellation/index.js
+++ frontend/src/components/InteractiveConstellation/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
+import Star from "./components/Star";
+import unselectedStar from "./assets/constellationstarunselected/constellationstarunselected.png";
+import selectedStar from "./assets/constellationstarselected/constellationstarselected.png";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   constellation: {
     position: "relative",
     height: "100%",
@@ -12,55 +15,6 @@ const useStyles = makeStyles((theme) => ({
     maxHeight: "100%",
     height: "auto",
   },
-  star1: {
-    position: "absolute",
-    top: "-1%",
-    left: "0%",
-    height: "20%",
-    height: "20%",
-  },
-  star2: {
-    position: "absolute",
-    top: "80%",
-    left: "10%",
-    height: "20%",
-    height: "20%",
-  },
-  star3: {
-    position: "absolute",
-    top: "9%",
-    left: "26%",
-    height: "20%",
-    height: "20%",
-  },
-  star4: {
-    position: "absolute",
-    top: "69%",
-    left: "32%",
-    height: "20%",
-    height: "20%",
-  },
-  star5: {
-    position: "absolute",
-    top: "40%",
-    left: "41%",
-    height: "20%",
-    height: "20%",
-  },
-  star6: {
-    position: "absolute",
-    top: "51%",
-    left: "70%",
-    height: "20%",
-    height: "20%",
-  },
-  star7: {
-    position: "absolute",
-    top: "40%",
-    left: "92%",
-    height: "20%",
-    height: "20%",
-  },
 }));
 
 export default function Constellation(props) {
@@ -69,13 +23,13 @@ export default function Constellation(props) {
   return (
     <div className={classes.constellation} style={{ top: props.top, left: props.left }}>
       <img alt="constellation" src={require("./assets/constellationnostars/constellationnostars.png")} className={classes.constellationImage} />
-      <img alt="star 1" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star1} />
-      <img alt="star 2" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star2} />
-      <img alt="star 3" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star3} />
-      <img alt="star 4" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star4} />
-      <img alt="star 5" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star5} />
-      <img alt="star 6" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star6} />
-      <img alt="star 7" src={require("./assets/constellationstarunselected/constellationstarunselected.png")} className={classes.star7} />
+      <Star top="-1%" left="0%" star={unselectedStar} />
+      <Star top="80%" left="10%" star={unselectedStar} />
+      <Star top="9%" left="26%" star={unselectedStar} />
+      <Star top="69%" left="32%" star={unselectedStar} />
+      <Star top="40%" left="41%" star={unselectedStar} />
+      <Star top="51%" left="70%" star={unselectedStar} />
+      <Star top="40%" left="92%" star={unselectedStar} />
     </div>
   );
 }
