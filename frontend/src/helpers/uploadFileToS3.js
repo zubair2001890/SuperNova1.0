f00c1e5ca8c017f8b54b77cc99c34de486e724f0
Michael Nares <michaelnares@hotmail.co.uk>
2020-08-04 17:53:04 +0100

    The S3 upload helper can now accept a profile image, a project image and a lab note.

--- frontend/src/helpers/uploadFileToS3.js
+++ frontend/src/helpers/uploadFileToS3.js
@@ -1,37 +1,58 @@
-import {sendUpdateAccount} from '../store/slices/middlewareAPI/middlewareAPI';
+import { sendUpdateAccount } from '../store/slices/middlewareAPI/middlewareAPI';
+import { useDispatch } from 'react-redux';
+const dispatch = useDispatch();
 const AWS = require('aws-sdk');
-export default uploadFileToS3 = function(fileContent, id)
-{
+// Type is one of profileImage, projectImage, labNoteDocument
+export default uploadFileToS3 = function (fileContent, type, id) {
   const params = {
     Bucket: "supernova",
-    Key: "Image saved from user with id " + id,
-    Body: fileContent 
+    Key: "File saved from user/project with id " + id,
+    Body: fileContent
   };
 
   const s3 = new AWS.S3({
     // I thought it would would be more secure to store them as environment variables.
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY
   });
 
-  s3.upload(params, handleUpload);
-} 
+  s3.upload(params, funciton(err, data){
+    handleUpload(err, data, type)
+  });
+}
 
-function handleUpload(err, data)
-  {
-    if (err) {
-        throw err;
-      }
-      const location = data.location;
-      console.log("File uploaded successfully to " + location);
-      const { loginWithRedirect, isAuthenticated, logout, user, getAccessTokenSilently } = useAuth0();
-      if (isAuthenticated) {
-          getAccessTokenSilently({
-             audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
-             scope: "read:current_user",
-             // So in the below code the imageURL property of the given user should be updated with the ID.
-          }).then(authToken => dispatch(sendUpdateAccount({ data: { imageURL: location }, authToken: authToken })));
-      }
+function handleUpload(err, data, type) {
+  if (err) {
+    throw err;
   }
-  
+  const location = data.location;
+  console.log("File uploaded successfully to " + location);
+  const { loginWithRedirect, isAuthenticated, logout, user, getAccessTokenSilently } = useAuth0();
+  if (isAuthenticated) {
+    switch (type) {
+      case "profileImage":
+        getAccessTokenSilently({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        }).then(authToken => dispatch(sendUpdateAccount({ data: { imageURL: location }, authToken: authToken })));
+        break;
+
+      case "projectImage":
+        getAccessTokenSilently({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        }).then(authToken => dispatch(sendUpdateProject({ data: { projectImage: location }, authToken: authToken })));
+
+      case "labNoteDocument": // It can be handled the same whether it's text or an image.
+        getAccessTokenSilently({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        }).then(authToken => dispatch(sendUpdateProject({ data: { labNotes: location }, authToken: authToken })));
+      default:
+        throw new Error("The String parameter passed in was not of the correct type.");
+    }
+
+  } // Ends the if block for if isAuthenticated()
+}
+
 
