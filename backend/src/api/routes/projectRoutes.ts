6978128edba0009bc45c2780e5db8ae0364dd7eb
Michael Nares <michaelnares@hotmail.co.uk>
2020-08-09 19:42:06 +0100

    Changing the delete project by scientist endpoint to private.

--- backend/src/api/routes/projectRoutes.ts
+++ backend/src/api/routes/projectRoutes.ts
@@ -8,7 +8,7 @@ export default (publicApi: Express.Router, privateApi: Express.Router) => {
   publicApi.route('/subfield/:field_name').get(projectsController.subFieldsByFieldName);
   publicApi.route('/project').get(projectsController.project);
   publicApi.route('/project/:project_id').get(projectsController.projectByProjectID);
-  publicApi.route('/deleteprojectbyscientist/:project_id').post(projectsController.deleteProjectByScientist);
+  privateApi.route('/deleteprojectbyscientist/:project_id').post(projectsController.deleteProjectByScientist);
   privateApi.route('/createproject').put(projectsController.createProject);
   privateApi.route('/updateproject/:project_id').post(projectsController.updateProject);
   privateApi.route('/updateprojectstatus/:project_id').post(projectsController.updateProjectStatus);
