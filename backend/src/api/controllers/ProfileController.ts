b6e83f24c67bddbe05f6dcbc9b015eed08eac81b
Michael Nares <michaelnares@hotmail.co.uk>
2020-08-09 16:45:02 +0100

    Uploading a new backend endpoint to upload files to S3.

--- backend/src/api/controllers/ProfileController.ts
+++ backend/src/api/controllers/ProfileController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import {UserAccount} from '../models/UserAccount';
 import {getProfileByID} from '../mongoQueries'
 import * as jwt_decode from 'jwt-decode';
+import AWS from 'aws-sdk';
+import uuid from 'uuid/v1';
 
 class ProfileController {
     constructor() {
@@ -44,6 +46,23 @@ class ProfileController {
         const profile = await getProfileByID(req.params.profile_sub);
         res.send(profile);
     }
-}
+
+    public upload = async (req: Request, res: Response) => {
+        const s3 = new AWS.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESS_KEY
+        });
+        const key = '${req.body.id}/${uuid()}.' + req.body.extension;
+        s3.getSignedUrl(
+            'putObject',
+            {
+              Bucket: 'supernova', // The supernova bucket has been created already.
+              ContentType: req.body.contentType,
+              Key: key
+            },
+            (err, url) => res.send({ key, url })
+          );
+        }
+    }
 
 export default ProfileController
