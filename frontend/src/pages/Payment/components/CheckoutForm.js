fb98ac18453b58990df9e130f3f33c0e854112bd
Michael Nares <michaelnares@hotmail.co.uk>
2020-08-11 21:02:09 +0100

    Sorting out a couple of merge conflicts and using a map instead for index.js of the payment page.

--- frontend/src/pages/Payment/components/CheckoutForm.js
+++ frontend/src/pages/Payment/components/CheckoutForm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { withStyles } from "@material-ui/core";
@@ -16,21 +15,6 @@ import { postSendPayment } from "../../../store/slices/middlewareAPI/fetchAPI";
 const styles = (theme) => ({
   formContainer: {
     padding: theme.spacing(10),
-=======
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import CardSection from "./CardSection";
-import {sendPayment} from "../../../store/slices/middlewareAPI/middlewareAPI"
-import axios from "axios";
-import {useDispatch} from "react-redux"
-//import { useAuth0 } from "@auth0/auth0-react";
-//const { loginWithRedirect, isAuthenticated, logout, user, getAccessTokenSilently } = useAuth0();
-
-const useStyles = makeStyles((theme) => ({
-  formContainer: {
-    paddingBottom: theme.spacing(10),
->>>>>>> e18215d9461e755913ad45c8e13bdd57d75c14ea
   },
   form: {
     height: "100%",
@@ -44,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     fontSize: 16,
     height: 50,
-<<<<<<< HEAD
   },
 });
 
@@ -143,82 +126,3 @@ const FormWithElementsProvider = (props) => {
 };
 
 export default FormWithElementsProvider;
-=======
-  }
-}));
-
-const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
-  const classes = useStyles();
-  const [isProcessing, setProcessingTo] = useState(false);
-  const [checkoutError, setCheckoutError] = useState();
-
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const handleFormSubmit = async (ev) => {
-    ev.preventDefault();
-
-    const billingDetails = {
-      //   name: ev.target.name.value,
-      //   email: ev.target.email.value,
-      //   address: {
-      //     city: ev.target.city.value,
-      //     line1: ev.target.address.value,
-      //     state: ev.target.state.value,
-      //     postal_code: ev.target.zip.value,
-      //   },
-      name: "John Doe",
-      email: "example@domain.com",
-      address: {
-        city: "City",
-        line1: "Address",
-        state: "State",
-        postal_code: "ZIP",
-      },
-    };
-
-    setProcessingTo(true); // So set processing to true when the transaction is happening.
-    /*const dispatch = useDispatch();
-    if (isAuthenticated) {
-      getAccessTokenSilently({
-         audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
-         scope: "read:current_user",
-      })
-      .then(authToken => dispatch(sendPayment({ data: { amount: price * 100 }, authToken: authToken })));
-    }
-    */
-    //const { data: clientSecret } = await axios.post("/api/payment_intents", {
-      //amount: price * 100, //Price is in the lowest denomination, the price is one of the input props for the CheckoutForm
-    //});
-    //console.log("clientSecret = "+ clientSecret);
-
-    /*const cardElement = elements.getElement(CardElement);
-
-    const paymentMethodReq = await stripe.createPaymentMethod({
-      type: "card",
-      card: cardElement,
-      billing_details: billingDetails,
-    });
-
-    console.log(paymentMethodReq);
-    const confirmCardPayment = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: paymentMethodReq.paymentMethod.id,
-    });
-
-    onSuccessfulCheckout();
-    console.log(confirmCardPayment); // This object should have a field status, with a value of "succeeded".
-    */
-  };
-
-  return (
-    <div className={classes.formContainer}>
-      <form onSubmit={handleFormSubmit} className={classes.form}>
-        <CardSection />
-        <button className={classes.pledgeButton}>Pledge</button>
-      </form>
-    </div>
-  );
-};
-
-export default CheckoutForm;
->>>>>>> e18215d9461e755913ad45c8e13bdd57d75c14ea
