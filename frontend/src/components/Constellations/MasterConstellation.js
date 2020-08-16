f76cff6cd09565a774d1aa394112713b9a413c79
carrot <reeshul@hotmail.com>
2020-08-16 16:53:19 +0100

    Removed positional  props from MasterConstellation

--- frontend/src/components/Constellations/MasterConstellation.js
+++ frontend/src/components/Constellations/MasterConstellation.js
@@ -19,10 +19,7 @@ export default (props) => {
 
   return (
     <RouterLink to={{ pathname: props.path, state: { title: props.subfield } }}>
-      <div
-        className={classes.constellation}
-        style={{ top: props.top, left: props.left }}
-      >
+      <div className={classes.constellation}>
         <div className={classes.constellationImage} />
         <div className={classes.subfieldName}>{props.subfield}</div>
       </div>
