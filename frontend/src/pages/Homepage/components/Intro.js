e96bbd3844efaf59b989f823eb0e7c8a9f243a46
AkThao <akaash.thao@gmail.com>
2020-09-14 11:26:11 +0100

    Change CREATE and EXPLORE text to buttons

--- frontend/src/pages/Homepage/components/Intro.js
+++ frontend/src/pages/Homepage/components/Intro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { makeStyles, useTheme, Typography } from "@material-ui/core";
+import { makeStyles, useTheme, Typography, Button } from "@material-ui/core";
 import GrowVerticalBar from "./GrowVerticalBar";
 import AnimateArrows from "./AnimatedArrows";
 
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.common.white,
     animation: "slideFadeUp 1.5s ease 1s backwards",
     fontSize: 100,
-    '@media (max-width:1000px)': {
+    "@media (max-width:1000px)": {
       fontSize: 70,
     },
   },
@@ -30,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     animation: "slideFadeUp 1.5s ease 2s backwards",
     fontSize: 45,
     textAlign: "center",
-    '@media (max-width:1000px)': {
+    "@media (max-width:1000px)": {
       fontSize: 30,
     },
   },
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     animation: "slideFadeUp 1.5s ease 2s backwards",
     fontSize: 35,
-    '@media (max-width:1000px)': {
+    "@media (max-width:1000px)": {
       fontSize: 20,
     },
   },
@@ -51,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
   },
   arrows: {
     animation: "slideFadeUp 1.5s ease 2s backwards",
-  }
+  },
 }));
 
 export default ({ scrollY, className }) => {
@@ -72,26 +72,38 @@ export default ({ scrollY, className }) => {
         </>
       </Typography>
       <div className={classes.createExploreContainer}>
-        <Typography
-          variant="h5"
-          style={{
-            color: "white",
-            textAlign: "right",
-            marginBottom: createExploreMarginBottom,
-          }}
-          className={classes.createExplore}
+        <Button
+          style={{ color: "white", marginBottom: createExploreMarginBottom }}
         >
-          <span style={{ flex: "0 0 50%", textAlign: "right" }}>CREATE</span>
-          <GrowVerticalBar
-            scrollY={scrollY}
-            className={classes.growVerticalBar}
-          />
-          <span style={{ flex: "0 0 50%", textAlign: "left" }}>EXPLORE</span>
-        </Typography>
-        <AnimateArrows
-          className={classes.arrows}
-          style={{ opacity: 1 - scrollY / createExploreMarginBottom }}
+          <Typography
+            variant="h5"
+            style={{
+              color: "white",
+              textAlign: "right",
+            }}
+            className={classes.createExplore}
+          >
+            CREATE
+          </Typography>
+        </Button>
+        <GrowVerticalBar
+          scrollY={scrollY}
+          className={classes.growVerticalBar}
         />
+        <Button
+          style={{ color: "white", marginBottom: createExploreMarginBottom }}
+        >
+          <Typography
+            variant="h5"
+            style={{
+              color: "white",
+              textAlign: "right",
+            }}
+            className={classes.createExplore}
+          >
+            EXPLORE
+          </Typography>
+        </Button>
       </div>
     </div>
   );
